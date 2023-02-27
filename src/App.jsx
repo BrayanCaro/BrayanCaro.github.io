f40c5566ea27ea97014c3d0c02872d046c50d7d4
Brayan Martínez Santana <brayanmtz@ciencias.unam.mx>
2023-02-27 06:54:44 -0600

    feat: Remove typewritter animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import './App.css';
+import "./App.css";
 import Sections from "./components/Sections";
 import Section from "./components/Section";
-import Writer from "./components/Writer";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import About from "./components/About";
 
 function App() {
-    return (
-        <div className="App">
-            <nav></nav>
-            <Sections>
-                <Section className="container">
-                    <div className="first jb-mono-font">
-                        <Writer/>
-                        <p className="presentation" style={{margin: 'auto'}}>
-                            I'm <span className="instance-member">Brayan</span>
-                            <br/>
-                            <small>Student <span className="instance-function">&&</span> Developer</small>
-                        </p>
-                    </div>
-                </Section>
-                <h2 id="projects" className="title container">Projects</h2>
-                <Projects/>
-                <h2 id="about" className="title container">About</h2>
-                <About/>
-            </Sections>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Sections>
+        <Section className="container p-3 sm:p-6 !min-h-screen">
+          <div className="text-lg">
+            <p className="text-6xl">
+              I'm <span className="instance-member">Brayan</span>
+            </p>
+            <p>
+              Student <span className="instance-function">&</span> Developer
+            </p>
+          </div>
+        </Section>
+        <h2 id="projects" className="title container">
+          Projects
+        </h2>
+        <Projects />
+        <h2 id="about" className="title container">
+          About
+        </h2>
+        <About />
+      </Sections>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
