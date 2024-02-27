e879ded0b00c0b5d2389fbdcdcd744196331fee5
Brayan <41640423+BrayanCaro@users.noreply.github.com>
2024-02-27 17:02:46 -0600

    fix(ui): Fix horizontal overflow for small devices (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Welcome from "./components/Sections/Welcome";
 
 function App() {
   return (
-    <div className="grid grid-cols-12 gap-12 p-12 grid-rows-[calc(100svh_-_theme(spacing.24))] min-h-svh">
+    <div className="grid grid-cols-12 gap-6 sm:gap-12 p-6 sm:p-12 grid-rows-[calc(100svh_-_theme(spacing.12))] sm:grid-rows-[calc(100svh_-_theme(spacing.24))] min-h-svh">
       <Welcome />
       <Footer />
     </div>
